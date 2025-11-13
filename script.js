03242519affe30b48a9303c3c49d4655e4fe4491
Utkarsh102002 <128974569+Utkarsh102002@users.noreply.github.com>
2025-11-13 15:01:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 //your JS code here. If required.
-const inputs = document.querySelectorAll('code')
+const inputs = document.querySelectorAll('.code')
 
-inputs.forEach((input,index)=>{
-	input.addEventListener('input',(e)=>{
+inputs[0].focus();
+
+
+//forward typing
+inputs.forEach((input, index)=>{
+	//handle the input event
+	input.addEventListener('input', (e)=>{
 		const value = e.target.value;
 
+		//only allow one digit
 		if(value.length>1){
-			e.target.value = value.slice(0,1);
+			e.target.value = value.slice(0,1)
 		}
-		if(value.length===1&&index<inputs.length-1){
-			input[index+1].focus();
+
+		if(value.length===1 && index<inputs.length-1){
+			inputs[index+1].focus();
 		}
 	})
-   input.addEventListener("keydown",(e)=>{
-	  if(e.key="Backspace" && input.value = "" && index>0){
-		  input[index-1].focus();
-		  input[index-1].value = "";
-	  }
-   })
+
+	//handling keydown for backspace
+	input.addEventListener('keydown', (e)=>{
+		if(e.key=== 'Backspace'){
+			if(e.target.value === '' && index>0){
+				e.preventDefault()
+				inputs[index-1].focus();
+				
+			}else{
+				e.target.value="";
+			}
+		}
+	})
+
 
 	
-})
 
+})
