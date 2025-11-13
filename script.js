372699582de7e3574712d8c71080b2249adc274d
Utkarsh102002 <128974569+Utkarsh102002@users.noreply.github.com>
2025-11-13 15:07:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
-//your JS code here. If required.
-const inputs = document.querySelectorAll('.code')
+const codes = document.querySelectorAll(".code");
 
-inputs[0].focus();
+codes[0].focus();
 
+codes.forEach((code, index) => {
 
-//forward typing
-inputs.forEach((input, index)=>{
-	//handle the input event
-	input.addEventListener('input', (e)=>{
-		const value = e.target.value;
+    code.addEventListener("input", (e) => {
+        const value = e.target.value;
 
-		//only allow one digit
-		if(value.length>1){
-			e.target.value = value.slice(0,1)
-		}
+        // Only keep 1 digit
+        if (value.length > 1) {
+            e.target.value = value.slice(0, 1);
+        }
 
-		if(value.length===1 && index<inputs.length-1){
-			inputs[index+1].focus();
-		}
-	})
+        // Move forward if not last input
+        if (value !== "" && index < codes.length - 1) {
+            codes[index + 1].focus();
+        }
+    });
 
-	//handling keydown for backspace
-	input.addEventListener('keydown', (e)=>{
-		if(e.key=== 'Backspace'){
-			if(e.target.value === '' && index>0){
-				e.preventDefault()
-				inputs[index-1].focus();
-				
-			}else{
-				e.target.value="";
-			}
-		}
-	})
-
-
-	
-
-})
+    code.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace") {
+            if (codes[index].value === "") {
+                if (index > 0) {
+                    codes[index - 1].value = "";
+                    codes[index - 1].focus();
+                }
+            } else {
+                codes[index].value = "";
+            }
+        }
+    });
+});
