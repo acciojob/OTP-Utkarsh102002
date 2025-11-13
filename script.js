d58dbc6d64743f4cbfa8ee053e13b4f838acfa56
Utkarsh102002 <128974569+Utkarsh102002@users.noreply.github.com>
2025-11-13 15:15:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 const codes = document.querySelectorAll(".code");
 
-codes[0].focus();
+document.getElementById("code-1").focus();
 
+// FORWARD MOVEMENT
 codes.forEach((code, index) => {
 
     code.addEventListener("input", (e) => {
         const value = e.target.value;
 
-        // Only one digit allowed
         if (value.length > 1) {
             code.value = value.slice(0, 1);
         }
 
         if (value !== "" && index < codes.length - 1) {
-            codes[index + 1].focus();
+            document.getElementById(`code-${index + 2}`).focus();
         }
     });
 
+    // BACKSPACE MOVEMENT
     code.addEventListener("keydown", (e) => {
         if (e.key === "Backspace") {
-            if (code.value === "" && index > 0) {
-                codes[index - 1].value = "";
-                codes[index - 1].focus();
+            if (code.value === "") {
+                if (index > 0) {
+                    document.getElementById(`code-${index}`).focus();
+                    document.getElementById(`code-${index}`).value = "";
+                }
+            } else {
+                code.value = "";
             }
         }
     });
